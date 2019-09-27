a313351bcfa3dae62fef306092a6556f0d835cd2
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-09-27 18:27:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,6 +43,5 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   else hash[key] = true 
     hash[key] += 1 
-    return hash[key]
-  end
+    return hash
 
